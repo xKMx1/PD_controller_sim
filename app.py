c4c58f2a2d5aae4be151878784b09915399a1cdc
Kamil Ratajczyk <kamilratajczyk00@gmail.com>
2024-04-13 19:33:56 +0200

    add frame

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-from tkinter import Tk
+from tkinter import *
+from tkinter import ttk
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+root = Tk()
+frm = ttk.Frame(root, padding=100)
+frm.grid()
+
+fig, (ax, bx) = plt.subplots(2, 1)
+ax.plot([1,2,3,4], [1,2,3,4])
+bx.plot([1,2,3,4], [3,4,6,2])
+
+
+canvas = FigureCanvasTkAgg(fig, root)
+canvas.draw()
+# plt.show()
+
+# root.mainloop()
