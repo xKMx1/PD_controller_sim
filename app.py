f2a7bc8c750c6ec1e489861a047b75ca822f8cbb
MaciejPita <120200536+MaciejPita@users.noreply.github.com>
2024-05-19 20:23:09 +0200

    change differential equation to the correct one

--- app.py
+++ app.py
@@ -20,12 +20,22 @@ y = np.zeros(total)  # sygnał wyjściowy
 y1p = np.zeros(total)  # pierwsza pochodna sygnału wyjściowego
 y2p = np.zeros(total)  # druga pochodna sygnału wyjściowego
 
-a2 = 0
-a1 = 1
-a0 = 2
-b2 = 3
-b1 = 2
-b0 = 1
+
+a1 = 4
+a0 = 20
+b2 = 300
+b1 = 149
+b0 = 69
+kd = 20
+kp = 4
+
+
+A1 = -(b1+kd*a0+kp*a1)/(b2+kd*a1)
+A0 = (b0+kp*a0)/(b2+kd*a1)
+B2 = (kd*a1)/(b2+kd*a1)
+B1 = (kd*a0+kp*a1)/(b2+kd*a1)
+B0 = (kp*a0)/(b2+kd*a1)
+
 
 # a1 = float(input("\n a1 = "))
 # a0 = float(input("\n a0 = "))
@@ -45,7 +55,7 @@ for i in range(total):
     u2p[i] = -M * w * w * np.sin(w * t)
 
 for i in range(total - 1):
-    y2p[i] = -a2 * y2p[i] - a1 * y1p[i] - a0 * y[i] + b2 * u2p[i] + b1 * u1p[i] + b0 * u[i]
+    y2p[i] = -A1 * y1p[i] - A0 * y[i] + B2 * u2p[i] + B1 * u1p[i] + B0 * u[i]
     y1p[i + 1] = y1p[i] + h * y2p[i]
     y[i + 1] = y[i] + h * y1p[i] + (h * h / 2.0) * y2p[i]
 
@@ -55,8 +65,8 @@ frm = ttk.Frame(root, padding=100)
 frm.grid()
 
 fig, (ax, bx) = plt.subplots(2, 1)
-ax.plot(time, y1p)
-bx.plot(time, y2p)
+ax.plot(time, u)
+bx.plot(time, y)
 
 
 canvas = FigureCanvasTkAgg(fig, root)
